e8b6fcbb903335fe50063d2ad2be7bf1047f0f39
Henry4593 <jonyango4@gmail.com>
2024-11-12 21:29:07 +0300

    initial commit

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+const request = require('request');
+const { promisify } = require('util');
+const id = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+// Promisify the request function
+const requestPromise = promisify(request);
+
+// Function to fetch and parse JSON data from a URL
+const fetchJson = async (url) => {
+    const response = await requestPromise(url);
+    if (response.statusCode !== 200) {
+        throw new Error(`Failed to fetch ${url}: ${response.statusMessage}`);
+    }
+    return JSON.parse(response.body);
+};
+
+const fetchCharacters = async () => {
+    try {
+        // Fetch the film data
+        const filmData = await fetchJson(apiUrl);
+        const characterUrls = filmData.characters;
+
+        // Fetch all character names in parallel
+        const characterPromises = characterUrls.map(url => fetchJson(url).then(data => data.name));
+        const characterNames = await Promise.all(characterPromises);
+
+        // Log each character's name
+        characterNames.forEach(name => console.log(name));
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+};
+
+fetchCharacters();
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,40 @@
+# Star Wars API
+
+This project involves interacting with the Star Wars API to retrieve and display information about Star Wars characters and films.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Files](#files)
+- [Author](#author)
+
+## Installation
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/Henry4593/alx-interview.git
+    ```
+2. Navigate to the project directory:
+    ```bash
+    cd alx-interview/0x06-starwars_api
+    ```
+3. Install the required dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+To use the Star Wars API script, run the following command:
+```bash
+node 0-starwars_characters.js "movie_id"
+```
+
+## Files
+
+- `0-starwars_characters.js`: Main script to interact with the Star Wars API.
+- `README.md`: Project documentation.
+
+## Author
+
+- **Joseph Otieno** - [GitHub](https://github.com/Henry4593)
